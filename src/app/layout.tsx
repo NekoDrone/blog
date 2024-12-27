0bbeae3b336a8fdd5f8e7ab891b4809e61a3179f
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-12-27 13:52:31 +0800

    chore: lint, prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,8 @@
 "use client";
 
-import { Geist, Geist_Mono, Lexend } from "next/font/google";
+import { Lexend } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from "react";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const lexend = Lexend({
   subsets: ["latin"],
